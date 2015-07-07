96423e9950ecf6c653d9ae2715d1ea4072006515
lachama <davilaclaudial@gmail.com>
2015-07-07 06:51:46 -0500

    Solucionar problema con el scope

--- src/js/controllers/adicionar.controller.js
+++ src/js/controllers/adicionar.controller.js
@@ -1,20 +1,23 @@
 angular.module('starter.controllers')
 
 .controller('AdicionarController', function($scope) {
-  //var scope = $scope;
-  //scope.formCandy ={};
+  // var scope = this;
+
+  // console.log("-----------2");
+  // console.log(scope);
   $scope.listCandies = [];
 
   $scope.addCandy = function() {
     console.log("1---------->");
-    console.log($scope.formCandy);
+    console.log($scope.formCandy.valorPaquete);
     $scope.listCandies.push($scope.formCandy);
+   
     console.log("2---------->");
     console.log($scope.listCandies);
     $scope.formCandy = "";
 
     
-    
+    //
     
    
   };
--- www/js/all.js
+++ www/js/all.js
@@ -124,20 +124,23 @@ angular.module('starter.controllers')
 angular.module('starter.controllers')
 
 .controller('AdicionarController', ["$scope", function($scope) {
-  //var scope = $scope;
-  //scope.formCandy ={};
+  // var scope = this;
+
+  // console.log("-----------2");
+  // console.log(scope);
   $scope.listCandies = [];
 
   $scope.addCandy = function() {
     console.log("1---------->");
-    console.log($scope.formCandy);
+    console.log($scope.formCandy.valorPaquete);
     $scope.listCandies.push($scope.formCandy);
+   
     console.log("2---------->");
     console.log($scope.listCandies);
     $scope.formCandy = "";
 
     
-    
+    //
     
    
   };
