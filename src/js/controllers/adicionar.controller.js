a7ac67cfaf7e27c76149ba35814143b00f6f3189
lachama <cdavila@cdavilamac.local>
2015-07-08 17:32:36 -0500

    Configurar las imágenes de la lista

--- src/js/controllers/adicionar.controller.js
+++ src/js/controllers/adicionar.controller.js
@@ -6,7 +6,7 @@ angular.module('starter.controllers')
   $scope.addCandy = function() {
     //$scope.listCandies.push($scope.formCandy);
     //$scope.formCandy = "";
-    $scope.formCandy.imgTipo === "porDefecto"?$scope.formCandy.imgTipo = "img/ionic.png":$scope.formCandy.imgTipo = "img/ionic.png";
+    $scope.formCandy.imgTipo === "porDefecto"?$scope.formCandy.imgTipo = "img/ionic.png":$scope.formCandy.imgTipo = "img/" + Math.floor((Math.random() * 5) + 1) + ".jpg";    
     obtenerLista.writeData($scope.formCandy);
     $scope.formCandy = "";
   };
